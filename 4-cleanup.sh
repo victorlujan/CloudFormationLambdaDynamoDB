0ae7a25fa924df7c8fd4f7602471f5cdd4b26edf
Victor Lujan <campillovictor@live.com>
2022-04-23 22:38:52 +0200

    update

--- 4-cleanup.sh
+++ 4-cleanup.sh
@@ -10,9 +10,9 @@ aws cloudformation delete-stack --stack-name $STACK
 echo "Deleted $STACK stack."
 
 if [ -f bucket-name.txt ]; then
-    ARTIFACT_BUCKET=$(cat bucket-name.txt)
-    echo "Borrando $ARTIFACT_BUCKET"
-     aws s3 rb --force s3://$ARTIFACT_BUCKET 
+    
+    echo "Borrando $BUCKET_NAME"
+     aws s3 rb --force s3://$BUCKET_NAME 
      rm bucket-name.txt
 
     
